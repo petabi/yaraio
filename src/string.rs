14dd798c5950dff80fe0baf7d4929146348866bb
Min Kim <msk@dolbo.net>
2018-11-29 14:59:52 -0800

    Support regexp as rule string
    Resolves: #6

--- src/string.rs
+++ src/string.rs
@@ -5,6 +5,24 @@ mod tests {
     use super::yara;
 
     #[test]
+    fn regexp() {
+        assert!(
+            yara::RulesParser::new()
+                .parse(
+                    r#"
+rule test
+{
+    strings:
+        $hello = /Hello/
+    condition:
+        true
+}
+"#
+                ).is_ok()
+        );
+    }
+
+    #[test]
     fn text_string() {
         assert!(
             yara::RulesParser::new()
