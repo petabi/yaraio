4fc387b2624a933593743b40c0506b113ca90f45
Min Kim <msk@dolbo.net>
2018-11-28 21:00:02 -0800

    Add a parser for minimal Yara rules (#2)
    The first step toward a full Yara rule parser. The grammar file can
    verify the simplest rule only. It needs to be extended to cover other
    rules.
    
    Resolves: #1

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,36 @@
+#[macro_use]
+extern crate lalrpop_util;
 
+lalrpop_mod!(pub yara);
+
+#[cfg(test)]
+mod tests {
+    use super::yara;
+
+    #[test]
+    fn empty() {
+        assert!(yara::RulesParser::new().parse("").is_ok());
+    }
+
+    #[test]
+    fn minimal_rule() {
+        assert!(
+            yara::RulesParser::new()
+                .parse("rule dummy { condition: true }")
+                .is_ok()
+        );
+    }
+
+    #[test]
+    fn multiple_rules() {
+        let rules = yara::RulesParser::new()
+            .parse(
+                "
+rule r1 { condition: true }
+rule r2 { condition: false }
+rule r3 { condition: true }
+",
+            ).unwrap();
+        assert_eq!(rules.len(), 3);
+    }
+}
